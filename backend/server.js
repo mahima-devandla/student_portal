62adeac0ba2541f20d2f9d10dc16a687118abb7c
mahima-devandla <69299202+mahima-devandla@users.noreply.github.com>
2025-03-20 20:34:35 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const pool = require("./db"); // Import database connection
 require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const PORT = 5000;
